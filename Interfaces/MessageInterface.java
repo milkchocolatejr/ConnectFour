e2222618f269348f92702c80025abbeb4ab7b59e
milkchocolatejr <alexanderwilliams29@gmail.com>
2025-04-15 14:32:32 -0500

    Added message class

--- Interfaces/MessageInterface.java
+++
@@ -1,3 +0,0 @@
-public interface MessageInterface implements Serializable{
-    
-}
--- Project3Server/Project3Server/src/main/java/Message.java
+++ Project3Server/Project3Server/src/main/java/Message.java
@@ -1,19 +1,22 @@
 import java.io.Serializable;
 
 public class Message implements Serializable {
+    public boolean isMove;
+    public boolean isJoin;
+    public int moveCol;
+    public String username;
+    public String messageText;
     static final long serialVersionUID = 42L;
 
-    int recipient;
-    String message;
 
-    public Message(String input){
-        message = input;
+    public Message(boolean isMove, boolean isJoin, int moveCol, String username, String messageText) {
+        this.isMove = isMove;
+        this.isJoin = isJoin;
+        this.moveCol = moveCol;
+        this.username = username;
+        this.messageText = messageText;
     }
-    public Message(int rec, String input){
-        recipient = rec;
-        message = input;
-    }
-    public String toString(){
-        return message;
-    }
+
+    public toString(){return messageText;}
 }
+
