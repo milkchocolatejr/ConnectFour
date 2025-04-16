fb60611e9729d055d5c796081c7df68e20bc71fe
milkchocolatejr <alexanderwilliams29@gmail.com>
2025-04-16 18:37:38 -0500

    Refactored message class

--- Project3Server/Project3Server/src/main/java/Message.java
+++ Project3Server/Project3Server/src/main/java/Message.java
@@ -8,7 +8,25 @@ public class Message implements Serializable {
     public String messageText;
     static final long serialVersionUID = 42L;
 
-    public Message(String input) { messageText = input; }
+    public Message(String username, String messageText) {
+        this.messageText = messageText;
+        this.isMove = false;
+        this.isJoin = false;
+        this.username = username;
+    }
+    public Message(boolean isJoin, String username, String messageText) {
+        this.isJoin = isJoin;
+        this.messageText = messageText;
+        this.username = username;
+        this.isMove = false;
+    }
+    public Message(boolean isMove, int moveCol, String username, String messageText) {
+        this.isMove = isMove;
+        this.moveCol = moveCol;
+        this.username = username;
+        this.messageText = messageText;
+        this.isJoin = false;
+    }
     public Message(boolean isMove, boolean isJoin, int moveCol, String username, String messageText) {
         this.isMove = isMove;
         this.isJoin = isJoin;
@@ -17,8 +35,8 @@ public class Message implements Serializable {
         this.messageText = messageText;
     }
 
+    @Override
     public String toString(){
         return messageText;
     }
 }
-
