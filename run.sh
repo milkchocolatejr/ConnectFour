4ca741267b7800b96295688cb58a9cdccf3f2d26
milkchocolatejr <alexanderwilliams29@gmail.com>
2025-04-21 09:33:11 -0500

    added multiple option in run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
+
+# Check if user provided the number of clients
+if [ -z "$1" ]; then
+    echo "Please provide the number of clients to run!"
+fi
+
+NUM_CLIENTS=$1
+
+# Run server
 mvn -f Project3Server/Project3Server/pom.xml clean compile exec:java &
-mvn -f Project3Client/Project3Client/pom.xml clean compile exec:java
+
+# Wait for server to init
+sleep 3
+
+# Start the specified number of clients
+for ((i = 1; i <= NUM_CLIENTS; i++))
+do
+  echo "Starting client $i"
+  mvn -f Project3Client/Project3Client/pom.xml clean compile exec:java &
+done
+
+# Wait for all background processes to finish
+wait
+
