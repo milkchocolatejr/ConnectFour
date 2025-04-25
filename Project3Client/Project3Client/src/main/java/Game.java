1b0abc64643ff46ba40b62ea6332fd7f6406bd68
tdcoleman127 <figgeryfirehead@gmail.com>
2025-04-24 23:12:18 -0500

    Still building outcome function for game, finished array of all possible outcomes

--- Project3Client/Project3Client/src/main/java/Game.java
+++ Project3Client/Project3Client/src/main/java/Game.java
@@ -195,6 +195,42 @@ public class Game {
                         {gameState[0][2], gameState[1][3], gameState[2][4], gameState[3][5]},
                         {gameState[0][3], gameState[1][4], gameState[2][5], gameState[3][6]},
 
+                        {gameState[1][0], gameState[2][1], gameState[3][2], gameState[4][3]},
+                        {gameState[1][1], gameState[2][2], gameState[3][3], gameState[4][4]},
+                        {gameState[1][2], gameState[2][3], gameState[3][4], gameState[4][5]},
+                        {gameState[1][3], gameState[2][4], gameState[3][5], gameState[4][6]},
+
+                        {gameState[2][0], gameState[3][1], gameState[4][2], gameState[5][3]},
+                        {gameState[2][1], gameState[3][2], gameState[4][3], gameState[5][4]},
+                        {gameState[2][2], gameState[3][3], gameState[4][4], gameState[5][5]},
+                        {gameState[2][3], gameState[3][4], gameState[4][5], gameState[6][6]},
+
+                        {gameState[3][0], gameState[4][1], gameState[5][2], gameState[6][3]},
+                        {gameState[3][1], gameState[4][2], gameState[5][3], gameState[6][4]},
+                        {gameState[3][2], gameState[4][3], gameState[5][4], gameState[6][5]},
+                        {gameState[3][3], gameState[4][4], gameState[5][5], gameState[6][6]},
+
+                        //All diagonal-left win outcomes
+                        {gameState[3][0], gameState[2][1], gameState[1][2], gameState[0][3]},
+                        {gameState[3][1], gameState[2][2], gameState[1][3], gameState[0][4]},
+                        {gameState[3][2], gameState[2][3], gameState[1][4], gameState[0][5]},
+                        {gameState[3][3], gameState[2][4], gameState[1][5], gameState[0][6]},
+
+                        {gameState[4][0], gameState[3][1], gameState[2][2], gameState[1][3]},
+                        {gameState[4][1], gameState[3][2], gameState[2][3], gameState[1][4]},
+                        {gameState[4][2], gameState[3][3], gameState[2][4], gameState[1][5]},
+                        {gameState[4][3], gameState[3][4], gameState[2][5], gameState[1][6]},
+
+                        {gameState[5][0], gameState[4][1], gameState[3][2], gameState[2][3]},
+                        {gameState[5][1], gameState[4][2], gameState[3][3], gameState[2][4]},
+                        {gameState[5][2], gameState[4][3], gameState[3][4], gameState[2][5]},
+                        {gameState[5][3], gameState[4][4], gameState[3][5], gameState[2][6]},
+
+                        {gameState[6][0], gameState[5][1], gameState[4][2], gameState[3][3]},
+                        {gameState[6][1], gameState[5][2], gameState[4][3], gameState[3][4]},
+                        {gameState[6][2], gameState[5][3], gameState[4][4], gameState[3][5]},
+                        {gameState[6][3], gameState[5][4], gameState[4][5], gameState[3][6]},
+
                 };
 
 
